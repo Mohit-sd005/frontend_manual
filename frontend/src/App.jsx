2ba62603f6a9ee061fa6ddb61ccc0d8be4cc19c8
Mohit-sd005 <mail2moht@gmail.com>
2025-08-01 22:31:32 +0530

    final-change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S109">
+    <Router basename="/frontend_manual">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
